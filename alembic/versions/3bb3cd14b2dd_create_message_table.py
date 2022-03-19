fa97e15ebcc679baa957fa246530ec1b1864e5b2
Russell Owen <rowen@uw.edu>
2022-03-19 10:46:25 -0700

    Add alembic support

---
+++ alembic/versions/3bb3cd14b2dd_create_message_table.py
@@ -0,0 +1,20 @@
+"""create message table
+
+Revision ID: 3bb3cd14b2dd
+Revises:
+Create Date: 2022-03-18 16:41:17.505723
+"""
+
+# revision identifiers, used by Alembic.
+revision = "3bb3cd14b2dd"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
